0ba01b0610998bd332c9af338f8a0db8927aeafd
Gosha Bar <anny1290008@gmail.com>
2022-07-28 13:34:47 +0300

    ViewModels

---
+++ MusicPlayer/MVVM/ViewsModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using MusicPlayer.MVVM.ViewsModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.MVVM.ViewsModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _NameTrack = "Music";
+
+        #region Name of the music track
+
+        /// <summary>
+        /// Свойство имяни музыкального трека
+        /// </summary>
+        public string NameTrack 
+        {
+            get => _NameTrack;
+            set
+            {
+                if(Equals(_NameTrack, value))
+                    return;
+                _NameTrack = value;
+                OnPropertyChenged();
+            }
+        }
+        #endregion
+
+
+
+    }
+}
