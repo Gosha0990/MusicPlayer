5e6032006db5eaac4a7a2ed21e055134b6265236
Gosha Bar <anny1290008@gmail.com>
2022-07-28 17:31:59 +0300

    Command play

---
+++ MusicPlayer/Infrastructure/MusicCommand/Base/IMusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.Infrastructure.MusicCommand.Base
+{
+    internal interface IMusicPlayer
+    {
+        bool IsPlaying { get; set; }
+        bool IsPaused { get; set; }
+
+        void PlayMusic();
+        void PauseMusic();
+        void NextMusic();
+        void PreviousMusic();
+    }
+}
---
+++ MusicPlayer/Infrastructure/MusicCommand/MusicPlayerCommand.cs
@@ -0,0 +1,50 @@
+﻿using MusicPlayer.Infrastructure.MusicCommand.Base;
+using System;
+using System.Media;
+using NAudio;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace MusicPlayer.Infrastructure.MusicCommand
+{
+    internal class MusicPlayerCommand : IMusicPlayer
+    {
+        public bool IsPlaying { get; set; }
+        public bool IsPaused { get; set; }
+
+        private string _path;
+        public MusicPlayerCommand(string path)
+        {
+            _path = path;
+        }
+        public void NextMusic()
+        {
+            
+        }
+
+        public void PauseMusic()
+        {
+            
+        }
+
+        
+        public void PlayMusic()
+        {
+            if (!string.IsNullOrEmpty(_path))
+            {
+                if (IsPlaying && !IsPaused)
+                {
+                    IWavePlayer waveOutDevice = new WaveOut();
+                    AudioFileReader audioFileReader = new AudioFileReader(_path);
+
+                    waveOutDevice.Init(audioFileReader);
+                    waveOutDevice.Play();
+                }
+            }
+        }
+
+        public void PreviousMusic()
+        {
+        }
+    }
+}
