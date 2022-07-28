ff3d6a90df098d7a5416de5bb34be8dac5f02672
Gosha Bar <anny1290008@gmail.com>
2022-07-28 14:02:06 +0300

    Command

---
+++ MusicPlayer/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,31 @@
+﻿using MusicPlayer.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+
+        private readonly Action<object> _Execute;
+        private readonly Func<object,bool> _CanExucute;
+        public LambdaCommand(Action<object> Execute, Func<object,bool> CanExecute)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute)); 
+            _CanExucute = CanExecute;
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return _CanExucute?.Invoke(parameter) ?? true;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _Execute(parameter);
+        }
+    }
+}
